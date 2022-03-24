86cc1284fa7acd86593204b8a400d37a65071530
Yurii <Urarat271@gmail.com>
2022-03-25 01:01:02 +0300

    add fuzzy search

--- search_text.py
+++ search_text.py
@@ -4,6 +4,8 @@ import re
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 import tensorflow as tf
 from nltk.tokenize import WhitespaceTokenizer
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 all_key = {
     "CONTRACT": [
@@ -40,13 +42,39 @@ model = None
 tokenizer = None
 path_to_model: str = "./doc-classification/"
 model_checkpoint: str = "sberbank-ai/ruRoberta-large"
+percentage_of_inaccurate_search: int = 90
+
+
+def get_key_from_json():
+    keys = []
+    json_file_with_key = open('keys_from_documents.json', encoding="utf8")
+    data = json.load(json_file_with_key)
+
+    for item in data:
+        item = str(item).replace('/', '|').replace('\n', '')
+        keys += re.split(r",|;", item)
+
+    for index, item in enumerate(keys):
+        if re.search(r'(\S+\|\S+)', item):
+            words = re.findall(r'\S+\|\S+', item)
+            for word in words:
+                key = keys[index]
+                word_split = word.split('|')
+                keys[index] = keys[index].replace(word, f"{word_split[0]}")
+                for sub_word in word_split[1:]:
+                    keys.append(key.replace(word, f"{sub_word}"))
+
+    return list(filter(None, keys))
+
+
+key_from_json = get_key_from_json()
 
 
 class list_of_sheets(enum.Enum):
     GOOD = 0
     BAD = 1
-    TEST = 2
-    TEST2 = 3
+    BAD2 = 2
+    GOOD2 = 3
 
 
 def wrapper(documents) -> [] or str:
@@ -88,7 +116,7 @@ def wrapper(documents) -> [] or str:
     return 'Bad result'
 
 
-def find_text(document, filename: str = None, path: str = None):
+def find_text(document, filename: str = "", path: str = ""):
     for ind, par in enumerate(document['paragraphs']):
         document['paragraphs'][ind]['paragraphBody']['text'] = re.sub('_+', '', par['paragraphBody']['text'])
         document['paragraphs'][ind]['paragraphHeader']['text'] = re.sub(' +', ' ', par['paragraphHeader']['text'])
@@ -96,10 +124,14 @@ def find_text(document, filename: str = None, path: str = None):
     if document['documentType'] == "CONTRACT" or document['documentType'] == "AGREEMENT":
         keys = all_key[document['documentType']]
 
-        index_of_paragraph, found_key = find_currency_header(document['paragraphs'], keys)
-        if index_of_paragraph >= 0:
-            paragraph = document['paragraphs'][index_of_paragraph]
-            return get_good_result(document, paragraph, path, filename, list_of_sheets.GOOD, found_key)
+        # index_of_paragraph, found_key, percentage = find_currency_header(document['paragraphs'], keys)
+        # if index_of_paragraph >= 0:
+        #     paragraph = document['paragraphs'][index_of_paragraph]
+        #     return get_good_result(document, paragraph, path, filename, list_of_sheets.GOOD, found_key, percentage)
+
+        paragraph = find_paragraph_by_keys(document, all_key[document['documentType']] + key_from_json, path, filename)
+        if paragraph is not None:
+            return paragraph, list_of_sheets.GOOD
 
         all_text = "".join('\n' + x['paragraphHeader']['text'] + '\n' + x['paragraphBody']['text'] for x in
                            document['paragraphs'])
@@ -137,7 +169,8 @@ def find_text(document, filename: str = None, path: str = None):
                         "length": len(text_from),
                         "textHeader": "\n".join(str(x['paragraphHeader']['text']) for x in document['paragraphs']),
                         "lengthHeader": sum(i['paragraphHeader']['length'] for i in document['paragraphs']),
-                        "key": found_key
+                        "key": found_key,
+                        "percentage": -2
                     }, list_of_sheets.GOOD)
 
         obj = find_let(document, filename=filename, path=path)
@@ -162,7 +195,7 @@ def find_text(document, filename: str = None, path: str = None):
         if document['documentType'] == "POWER_OF_ATTORNEY":
             paragraph = find_paragraph_by_keys(document, all_key[document['documentType']], path, filename)
             if paragraph is not None:
-                return paragraph, list_of_sheets.TEST2
+                return paragraph, list_of_sheets.GOOD2
 
         keys: [str] = [
             'Приказываю', 'Обязываю',
@@ -185,22 +218,22 @@ def find_text(document, filename: str = None, path: str = None):
             'Разлив',
             'отход',
         ]
-        keys += get_key_from_json()
+        keys += key_from_json
         paragraph = find_paragraph_by_keys(document, keys, path, filename)
         if paragraph is not None:
-            return paragraph, list_of_sheets.TEST2
+            return paragraph, list_of_sheets.GOOD2
 
         obj = find_let(document, filename=filename, path=path)
         if obj is not None:
-            return obj, list_of_sheets.TEST2
+            return obj, list_of_sheets.GOOD2
 
-        return get_bad_results(document, path, filename, list_of_sheets.TEST)
+        return get_bad_results(document, path, filename, list_of_sheets.BAD2)
 
 
 def find_paragraph_by_keys(document, keys: [str], path: str, filename: str):
     index = None
     currency_text: str = ''
-    index_of_paragraph, found_key = find_currency_header(document['paragraphs'], keys)
+    index_of_paragraph, found_key, percentage = find_currency_header(document['paragraphs'], keys)
 
     if index_of_paragraph >= 0:
         index = index_of_paragraph
@@ -208,13 +241,14 @@ def find_paragraph_by_keys(document, keys: [str], path: str, filename: str):
 
     if index_of_paragraph < 0:
         for i, p in enumerate(document['paragraphs']):
-            currency_text, found_key = find_currency_text(p, keys)
+            currency_text, found_key, percentage = find_currency_text(p, keys)
             if currency_text:
                 index = i
                 break
 
     if index is not None:
-        while currency_text and len(WhitespaceTokenizer().tokenize(currency_text)) < 300 and index != len(document['paragraphs']) - 1:
+        while currency_text and len(WhitespaceTokenizer().tokenize(currency_text)) < 300 and index != len(
+                document['paragraphs']) - 1:
             index += 1
             currency_text += document['paragraphs'][index]['paragraphBody']['text']
 
@@ -226,13 +260,14 @@ def find_paragraph_by_keys(document, keys: [str], path: str, filename: str):
             "length": len(currency_text),
             "textHeader": document['paragraphs'][index]['paragraphHeader']['text'],
             "lengthHeader": document['paragraphs'][index]['paragraphHeader']['length'],
-            "key": found_key
+            "key": found_key,
+            "percentage": percentage
         }
 
     return None
 
 
-def find_currency_header(paragraphs, keys: [str]) -> (int, int) or (int, str):
+def find_currency_header(paragraphs, keys: [str]) -> (int, int, int) or (int, str, int):
     for key in keys:
         for index_of_paragraph, paragraph in enumerate(paragraphs):
             header_text_in_low_reg = paragraph['paragraphHeader']['text'].lower()
@@ -241,34 +276,36 @@ def find_currency_header(paragraphs, keys: [str]) -> (int, int) or (int, str):
                 continue
             for key_from_good_keys in all_good_keys:
                 if key_from_good_keys.lower() in header_text_in_low_reg:
-                    return index_of_paragraph, key
-            if re.search(f"(?i)({key})", header_text_in_low_reg):
+                    return index_of_paragraph, key, 100
+            is_it_found, percentage = is_high_percentage(key, header_text_in_low_reg)
+            if is_it_found:
                 if 'Статья'.lower() in key.lower() and any(x.lower() in header_text_in_low_reg for x in all_bad_keys):
                     continue
                 if any(x.lower() in header_text_in_low_reg for x in all_bad_keys):
                     continue
                 if re.search("(:)\s*$", paragraph['paragraphBody']['text'].lower()):
-                    return -2, -1
-                return index_of_paragraph, key
+                    return -2, -1, percentage
+                return index_of_paragraph, key, percentage
 
-    return -1, -1
+    return -1, -1, 0
 
 
 def find_currency_text(paragraph, keys: [str]):
     basic_text_in_low_reg = paragraph['paragraphBody']['text'].lower()
     basic_text_in_low_reg = basic_text_in_low_reg.replace(',', '')
     if not basic_text_validation(paragraph):
-        return False, -1
+        return False, -1, 0
     for key in all_good_keys:
         if key.lower() in basic_text_in_low_reg:
-            return paragraph['paragraphBody']['text'], key
+            return paragraph['paragraphBody']['text'], key, 100
     for key in keys:
-        if re.search(f"(?i)({key})", basic_text_in_low_reg):
+        is_it_found, percentage = is_high_percentage(key, basic_text_in_low_reg)
+        if is_it_found:
             if any(x.lower() in basic_text_in_low_reg for x in all_bad_keys):
-                return False, -1
+                return False, -1, percentage
             # return ''.join(re.split(f"(?i)({key})", paragraph['paragraphBody']['text'])[1:])
-            return paragraph['paragraphBody']['text'], key
-    return False, -1
+            return paragraph['paragraphBody']['text'], key, percentage
+    return False, -1, 0
 
 
 def basic_text_validation(paragraph) -> bool:
@@ -276,7 +313,7 @@ def basic_text_validation(paragraph) -> bool:
     return len(basic_text_in_low_reg) >= 150 and len(WhitespaceTokenizer().tokenize(basic_text_in_low_reg)) >= 15
 
 
-def find_let(document, filename=None, document_type=None, path=None):
+def find_let(document, filename: str = "", document_type: str = "", path: str = ""):
     key_value = ['о нижеследующем:', 'нижеследующем:', 'о нижеследующем', 'нижеследующем']
     if document_type == 'SUPPLEMENTARY_AGREEMENT':
         key_value.append('в следующей редакции:')
@@ -285,15 +322,17 @@ def find_let(document, filename=None, document_type=None, path=None):
 
     for i, p in enumerate(document['paragraphs']):
         for index, key in enumerate(key_value):
-            if key.lower() in p['paragraphBody']['text'].lower() or key.lower() in p['paragraphHeader']['text'].lower():
+            is_it_found_in_text, percentage_of_text = is_high_percentage(key, p['paragraphBody']['text'])
+            is_it_found_in_header, percentage_of_header = is_high_percentage(key, p['paragraphHeader']['text'])
+            if is_it_found_in_text or is_it_found_in_header:
                 text = p['paragraphBody']['text']
                 text_header = p['paragraphHeader']['text']
-                list_of_tokenize_words: [str] = WhitespaceTokenizer().tokenize(text)
+                # list_of_tokenize_words: [str] = WhitespaceTokenizer().tokenize(text)
                 # text = ''.join(re.split(f"(?i)({x})", p['paragraphBody']['text'])[1:])
 
                 if len(document['paragraphs']) > 1:
                     d = i + 1
-                    while len(list_of_tokenize_words) < 300 and d < len(document['paragraphs']):
+                    while len(WhitespaceTokenizer().tokenize(text)) < 300 and d < len(document['paragraphs']):
                         text += document['paragraphs'][d]['paragraphBody']['text']
                         d += 1
                 return {
@@ -304,7 +343,8 @@ def find_let(document, filename=None, document_type=None, path=None):
                     "length": len(text),
                     "textHeader": text_header,
                     "lengthHeader": len(text_header),
-                    "key": key
+                    "key": key,
+                    "percentage": percentage_of_text if is_it_found_in_text else percentage_of_header
                 }
     return None
 
@@ -328,7 +368,10 @@ def remove_bad_symbols(text: str) -> str:
     return ' '.join(list_of_tokenize_words[:300])
 
 
-def get_bad_results(document, path, filename, sheet):
+def get_bad_results(document, path: str,
+                    filename: str,
+                    sheet: list_of_sheets,
+                    percentage: int = -2):
     return ({
                 "path": path,
                 "name": filename if not path else path.split("\\")[-1],
@@ -337,11 +380,16 @@ def get_bad_results(document, path, filename, sheet):
                 "length": sum(i['paragraphBody']['length'] for i in document['paragraphs']),
                 "textHeader": "\n+++++++++++++\n".join(x['paragraphHeader']['text'] for x in document['paragraphs']),
                 "lengthHeader": sum(i['paragraphHeader']['length'] for i in document['paragraphs']),
-                "key": -1
+                "key": -1,
+                "percentage": percentage
             }, sheet)
 
 
-def get_good_result(document, paragraph, path: str, filename: str, sheet: list_of_sheets, key: str):
+def get_good_result(document, paragraph,
+                    path: str, filename: str,
+                    sheet: list_of_sheets,
+                    key: str,
+                    percentage: int = -2):
     return ({
                 "path": path,
                 "name": filename if not path else path.split("\\")[-1],
@@ -350,22 +398,13 @@ def get_good_result(document, paragraph, path: str, filename: str, sheet: list_o
                 "length": paragraph['paragraphBody']['length'],
                 "textHeader": paragraph['paragraphHeader']['text'],
                 "lengthHeader": paragraph['paragraphHeader']['length'],
-                "key": key
+                "key": key,
+                "percentage": percentage
             }, sheet)
 
 
-def get_key_from_json():
-    keys = []
-    json_file_with_key = open('keys_from_documents.json', encoding="utf8")
-    data = json.load(json_file_with_key)
-
-    for item in data:
-        item = str(item).replace('/', '|').replace('\n', '')
-        keys += re.split(r",|;", item)
-
-    for index, item in enumerate(keys):
-        if re.search(r'(\S+\|\S+)', item):
-            match = re.findall(r'(\S+\|\S+)', item)
-            for word in match:
-                keys[index] = keys[index].replace(word, f"({word})")
-    return list(filter(None, keys))
+def is_high_percentage(key: str, text: str) -> (bool, int):
+    percentage = fuzz.partial_ratio(key.lower(), text.lower())
+    if percentage > percentage_of_inaccurate_search:
+        return True, percentage
+    return False, percentage
